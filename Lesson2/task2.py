e5351244fccbfbe6b284c94fff6bcb8287e6afba
serjio <serjiochekv@yandex.ru>
2021-02-04 10:58:09 +0300

    Задача 2 урока 6

---
+++ Lesson2/task2.py
@@ -0,0 +1,7 @@
+my_list = []
+my_tuple = ()
+my_dic = {}
+
+print(f'Это список - {type(my_list) == list}')
+print(f'Это кортеж - {type(my_tuple) == tuple}')
+print(f'Это словарь - {type(my_dic) == dict}')
